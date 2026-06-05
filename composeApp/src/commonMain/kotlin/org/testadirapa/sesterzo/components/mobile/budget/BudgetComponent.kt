6756430fcc1c5f4ab1c7e12eb65f7fc9a068df71
Vincenzo Pierro <vcp@icure.com>
2026-06-06 00:09:01 +0200

    Added scroll state to main page

--- composeApp/src/commonMain/kotlin/org/testadirapa/sesterzo/components/mobile/budget/BudgetComponent.kt
+++ composeApp/src/commonMain/kotlin/org/testadirapa/sesterzo/components/mobile/budget/BudgetComponent.kt
@@ -10,6 +10,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -90,7 +92,7 @@ fun BudgetComponent(
 		}.onFailure(onError)
 	}
 	Column(
-		modifier = Modifier.padding(scaffoldPadding)
+		modifier = Modifier.padding(scaffoldPadding).verticalScroll(rememberScrollState()),
 	) {
 		Spacer(modifier = Modifier.height(16.dp))
 		val budgetReference = budget.toReference()
